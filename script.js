482e8219a00d4ad6b5928151ec000d945e59588d
Oladipupo Samuel <samueloladipupo.sam@gmail.com>
2026-01-03 11:35:04 +0100

    Add loading screen and dark/light mode functionality

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+/* LOADING SCREEN */
+window.addEventListener("load", () => {
+    document.getElementById("loader").style.display = "none";
+});
+
+/* TYPING EFFECT */
 const text = "Upcoming Web Developer | HTML | CSS | JavaScript";
 let index = 0;
+const typing = document.querySelector(".typing");
 
 function typeEffect() {
-    const typing = document.querySelector(".typing");
     typing.textContent = text.slice(0, index);
     index++;
-
     if (index <= text.length) {
         setTimeout(typeEffect, 100);
     }
 }
-
 typeEffect();
+
+/* DARK / LIGHT MODE */
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("light");
+    toggle.textContent =
+        document.body.classList.contains("light") ? "☀️" : "🌙";
+});
